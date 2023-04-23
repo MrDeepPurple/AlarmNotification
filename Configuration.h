54f773f406c40bedeefab634c03c6aa717725d64
Marco Passeri <mbass.pass@gmail.com>
2023-04-23 15:26:55 +0200

    Added a generic configuration

---
+++ Configuration.h
@@ -0,0 +1,67 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+
+//--------------------- PIN config ------------------------//
+/************************************************************
+ * This section provides the relevant configuration for the  
+ * pins connected to the alarm main board. Currently two
+ * pins shall be connected
+ *
+ * ENABLED_ALARM_PIN is used to verify if the alarm is 
+ *      enabled or disabled. It shall be connected to to a
+ *      line in the main board which provides high voltage
+ *      when the alarm is enabled
+ *
+ * TRIGGERED_ALARM_PIN shall be connected to the alarm 
+ *      sounder in the main board 
+ *
+ ***********************************************************/
+#define ENABLED_ALARM_PIN 26
+#define TRIGGERED_ALARM_PIN 27 
+
+//--------------------- Wifi config ------------------------//
+/************************************************************
+ * This section provides the relevant configuration for the  
+ * wifi connection. In particular three strings shall be
+ * provided: SSID, KEY and HOSTNAME
+ *
+ ***********************************************************/
+// wifi network name (SSID)
+#define WIFI_SSID       "your-wifi-ssid"
+// wifi network password
+#define WIFI_KEY        "your-wifi-password"
+// hostname of the device in current wifi networs
+#define WIFI_HOSTNAME   "ESP32 Alarm Panel"
+
+//------------------- Telegram config ----------------------//
+/************************************************************
+ * This section provides the relevant configuration for the  
+ * telegram bot. It requires the token of the bot and a list
+ * of chat IDs to whom the notifications will be sent
+ *
+ ***********************************************************/
+// telegram bot token as provided by botfather  
+#define BOT_TOKEN "your-telegram-bot-token"
+// telegram chat id list (Currently only one chat is supported)
+#define CHAT_ID 00000000 // replace with your chat id
+
+//---------------- Notification messages -------------------//
+/************************************************************
+ * This section provides the strings related to the
+ * notification messages. Change them or translate them as
+ * you like.
+ *
+ ***********************************************************/
+#define STATUS_ON "Centrale abilitata"
+#define STATUS_OFF "Centrale disabilitata"
+#define ALERT_MESSAGE "Allarme in corso!"
+#define BOOT_MESSAGE "ESP32 riavviato e operativo"
+
+//--------------------- Misc config ------------------------//
+/************************************************************
+ * This section provides generic functionalities
+ ***********************************************************/
+// uncomment to enable serial debug
+//#define DBG_EN
+
+#endif
